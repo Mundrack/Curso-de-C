94857c00c9930015b7863ae147fc67132b02beb2
Mundrack <mateopuga75@gmail.com>
2023-07-07 14:57:36 -0500

    conclucion

---
+++ conclusion.c
@@ -0,0 +1,6 @@
+//CONCLUCION.
+/*Se aprendio el uzo de las estructuras con una convinacion de variables en funsionamiento con las funciones, las cosas vasicas fueron utiles para repaar y no olvidar y las cosas
+nuevas me ayudara a hacer los codigos de marena mas  consisa y de manera mas optima. Ademas la practuica de los codigos que se plantearon hacer me ayudo en la practica de como hacer 
+las cosas y no olvidar y para agilizar la fluides de como organizo todo. El conocer del malock, calock y de ralock fue confuso pero luego de practicar y ver como uno 
+ayuda a implementar enspacio sin cambiar los datos anteriores y que otro ayuda a colocar almacenamiento pero reubicando y otro es para aumentar mas espacio en cualquiera
+todo se uelve mas practico. En pocas aprendi cosas nuevas que son utiles y que aunque me confundi logre aprender y esto me ayudara a creser.*/
